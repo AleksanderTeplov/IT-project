59ea8e98ec22c8ab6e9be4a294c68f1cbb3025ea
Александр Теплов <74897344+AleksanderTeplov@users.noreply.github.com>
2020-11-23 17:06:53 +0900

    new site for organization
    IT - project

---
+++ script.js
@@ -0,0 +1,45 @@
+// Меню бургер
+const burgerMenuOpen = () => {
+	let elem = document.querySelector('.menu-window'),
+	menu = document.querySelector('.header__nav_list')
+	elem.style.display = 'block'
+	document.body.style.overflow = 'hidden'
+}
+const burgerMenuClose = () => {
+	let elem = document.querySelector('#burgerWindow')
+	elem.style.display = 'none'
+	document.body.style.overflow = 'scroll'
+}
+
+// Добавление-удаление соавтора в интернет-приемной
+const addedToCoauthor = () => {
+	$('.remove-to-coauthor__btn').css({'display':'block'})
+
+	let coauthor = document.querySelector('.coauthor').cloneNode(true)
+	coauthor.style.display = 'block'
+
+	let cloneNode = document.querySelector('.coauthors')
+	cloneNode.appendChild(coauthor)
+
+
+	let couter = 0
+
+	for (let i = 0; i < coauthor.length; i++){ 
+	  	counter += parseInt(coauthor[i].innerHTML);
+	}
+
+}
+
+const removeToLastCoauthor = () => {
+	$('.coauthor').last().remove()
+
+	if (!$('.coauthor').length) {
+		$('.remove-to-coauthor__btn').css({'display':'none'})
+		
+	}
+}
+
+
+
+
+	
